e6e5c1ea20cccec884e244f40ff80e3761286245
yuhei.sakai <sansan5505.s.f@gmail.com>
2020-10-04 17:04:02 +0900

    a

--- src/memo/Store.js
+++ src/memo/Store.js
@@ -11,7 +11,69 @@ const initData = {
 export function memoReducer(state = initData, action) {
   switch (action.type) {
     case 'ADD':
-      return addReduce(state, action);
+      return addRimport React, { Component } from 'react';
+      import { connect } from 'react-redux';
+      import { addMemo } from './Store';
+      
+      
+      class AddForm extends Component {
+        input = {
+          fontSize:"16pt",
+          color:"#006",
+          padding:"1px",
+          margin:"5px 0px"
+        }
+        btn = {
+          fontSize:"14pt",
+          color:"#006",
+          padding:"2px 10px"
+        }
+      
+        constructor(props){
+          super(props);
+          this.state = {
+            message:''
+          }
+          this.doChange = this.doChange.bind(this);
+          this.doAction = this.doAction.bind(this);
+        }
+      
+      
+        doChange(e){
+          this.setState({
+            message: e.target.value
+          });
+        }
+      
+      
+        doAction(e){
+          e.preventDefault();
+          let action = addMemo(this.state.message);
+          this.props.dispatch(action);
+          this.setState({
+            message: ''
+          });
+        }
+      
+      
+        render(){
+          return (
+            <div>
+              <p style={this.message}>{this.props.message}</p>
+              <form onSubmit={this.doAction}>
+              <input type="text" size="40" onChange={this.doChange}
+                style={this.input} value={this.state.message} required />
+              <input type="submit" style={this.btn} value="Add"/>
+              </form>
+            </div>
+          );
+        }
+      }
+      export default connect((state)=>state)(AddForm);
+      
+      
+      
+      educe(state, action);
 
     case 'DELETE':
       return deleteReduce(state, action);
